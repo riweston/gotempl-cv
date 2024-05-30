c79ffee888be79fa62b1e3c4c50a02d834584835
Richard Weston <github@riweston.io>
2024-05-30 10:40:20 +0100

    refactor: This will allow me in the future to rejig dev deployment vs prod

---
+++ dev.go
@@ -0,0 +1,19 @@
+//+build dev
+//go:build dev
+// +build dev
+
+package main
+
+import (
+	"net/http"
+
+	"github.com/a-h/templ"
+	"github.com/riweston/gotempl-cv/views/layout"
+)
+
+func httpHandler() {
+	// Serve the ./public directory.
+	http.Handle("/public/", http.StripPrefix("/public/", http.FileServer(http.Dir("public"))))
+	http.Handle("/", templ.Handler(layout.Layout()))
+	http.ListenAndServe("localhost:8080", nil)
+}
--- main.go
+++ main.go
@@ -1,15 +1,5 @@
 package main
 
-import (
-	"net/http"
-
-	"github.com/a-h/templ"
-	"github.com/riweston/gotempl-cv/views/layout"
-)
-
 func main() {
-	// Serve the ./public directory.
-	http.Handle("/public/", http.StripPrefix("/public/", http.FileServer(http.Dir("public"))))
-	http.Handle("/", templ.Handler(layout.Layout()))
-	http.ListenAndServe("localhost:8080", nil)
+	httpHandler()
 }
