fa0cde705d8ac30d65a4770d7d368c4bf5382374
Richard Weston <github@riweston.io>
2024-05-30 10:39:43 +0100

    feat: Add the work experience section

---
+++ pkgs/types/work_experience.go
@@ -0,0 +1,27 @@
+package types
+
+import (
+	"io"
+
+	"gopkg.in/yaml.v3"
+)
+
+type Job struct {
+	Company       string   `yaml:"company"`
+	LogoPath      string   `yaml:"logoPath"`
+	LinkedIn      string   `yaml:"linkedIn"`
+	Website       string   `yaml:"website"`
+	CurrentRole   string   `yaml:"currentRole"`
+	StartDate     string   `yaml:"startDate"`
+	EndDate       string   `yaml:"endDate"`
+	Description   string   `yaml:"description"`
+	PreviousRoles []string `yaml:"previousRoles"`
+}
+
+type WorkExperienceData struct {
+	Jobs []Job `yaml:"jobs"`
+}
+
+func (w *WorkExperienceData) ParseData(reader io.Reader) error {
+	return yaml.NewDecoder(reader).Decode(w)
+}
