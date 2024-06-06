76d4c57754b071772f87f7bed1243b460fcd09cc
Richard Weston <github@riweston.io>
2024-06-06 15:04:26 +0100

    refactor: Rationalised my entrypoint functions so they're a bit easier to find

--- entrypoint_dev.go
+++ entrypoint_dev.go
@@ -11,8 +11,9 @@ import (
 	"github.com/riweston/gotempl-cv/views/layout"
 )
 
-func httpHandler() {
+func entrypoint() {
 	// Serve the ./public directory.
+	log.Println("Running development server")
 	http.Handle("/public/", http.StripPrefix("/public/", http.FileServer(http.Dir("public"))))
 	http.Handle("/", templ.Handler(layout.Layout()))
 	http.ListenAndServe("localhost:8080", nil)
